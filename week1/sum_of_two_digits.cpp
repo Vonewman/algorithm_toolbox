eada05793eb879a6635daa2c7e05a1babe5cc7b9
vonewman <vonewman7@gmail.com>
2019-07-29 14:44:40 +0000

    Add sum of two digits

---
+++ week1/sum_of_two_digits.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+/* A program that gives you the sum of two digits*/
+int sum(int a, int b);
+int main()
+{
+    int num1, num2;
+    printf("Enter the 2 numbers num1 .and num2 \n");
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+    std::cout << sum(num1, num2) << std::endl;
+    return 0;
+}
+
+int sum(int a, int b)
+{
+    return a+b;
+}
