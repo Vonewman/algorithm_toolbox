9941ae265354c58525b01d4f511c0c8e48fff21c
Abdoulaye Diallo <vonewman7@gmail.com>
2019-08-02 19:12:30 +0000

    last digit of the sum

---
+++ week2_algorithic_warm-up/fibonacci_sum_last_digit.py
@@ -0,0 +1,19 @@
+def fibonacci_sum_last_digit(n):
+    if n <= 0:
+        return 0
+
+    fib = [int(i) for i in range(n+1)]
+
+    fib[0] = 0
+    fib[1] = 1
+
+    sum = fib[0] + fib[1]
+
+    for i in range(2, n+1):
+        fib[i] = fib[i-1] + fib[i-2]
+        sum += fib[i]
+
+    return sum % 10
+
+n = int(input())
+print(fibonacci_sum_last_digit(n))
