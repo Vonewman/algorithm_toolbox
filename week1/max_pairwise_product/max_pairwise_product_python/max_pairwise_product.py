02659c0312916285c6872c27ced8743811231010
vonewman <vonewman7@gmail.com>
2019-11-05 16:06:55 -0500

    add max_pairwise_product_python

--- week1/max_pairwise_product/max_pairwise_product_python/max_pairwise_product.py
+++ week1/max_pairwise_product/max_pairwise_product_python/max_pairwise_product.py
@@ -7,7 +7,21 @@ def max_pairwise_product(numbers):
 
     return max_product
 
-if __name__ == '__main__':
-    input_n = int(input())
-    input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+def max_pairwise_product_fast(numbers):
+    n = len(numbers)
+
+    max_index1 = -1
+    for i in range(n):
+        if ((max_index1 == -1) or (numbers[i] > numbers[max_index1])):
+            max_index1 = i
+
+    max_index2 = -1
+    for j in range(n):
+        if ((numbers[j] != numbers[max_index1]) and ((max_index2 == -1) or (numbers[j] > numbers[max_index2]))):
+            max_index2 = j
+
+    return (numbers[max_index1]) * numbers[max_index2]
+                                                                           
+
+numbers = [1, 2, 3]
+print("{}".format(max_pairwise_product(numbers)))
