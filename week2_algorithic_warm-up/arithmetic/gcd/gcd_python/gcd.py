acf0caa8475d58faddfb1bcee26c9a864fa08fae
vonewman <vonewman7@gmail.com>
2019-11-16 14:18:26 -0500

    reorganise some file

---
+++ week2_algorithic_warm-up/arithmetic/gcd/gcd_python/gcd.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+def gcd_fast(a, b):
+    if (b == 0):
+        return a
+
+    a_prime = a % b;
+    return gcd_fast(b, a_prime)
+
+if __name__ == '__main__':
+    input = stdin.readline()
+    a, b = map(int, input.split())
+    print("{}".format(gcd_fast(a, b)))  
