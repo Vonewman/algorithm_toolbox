1dd7f279a51ab503fdd7dae1b78bd031d16e2713
vonewman <vonewman7@gmail.com>
2020-04-02 16:52:24 -0400

    fractional_knapsack_problem solved

--- week3_greedy_algorithm/money_change/change.py
+++ week3_greedy_algorithm/money_change/change.py
@@ -1,8 +1,21 @@
 import sys
 
 def get_change(m):
+    """
+    The goal in this problem is to find the minimum
+    number of coins needed to change the input
+    (an integer) into with denominations 1, 5 and 10
+
+    -- Input Format: a single integer m.
+    -- Output Format: the minimum number of coins
+                      with denominations 1, 5, 10
+                      that changes m.
+    """
     # write your code here
-    return (m//10) + ((m%10)//5) + (m%5)
+
+    change = (m//10) + ((m%10)//5) + (m%5)
+    
+    return change
 
 if __name__ == '__main__':
     m = int(sys.stdin.readline())
