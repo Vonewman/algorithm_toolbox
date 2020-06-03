e1519b72acd6e7f9fc217c1b7d187045b1ad672e
vonewman <vonewman7@gmail.com>
2020-06-03 08:39:37 -0400

    add some changes to fibonacci.py

--- week2_algorithic_warm-up/fibonacci/fibonacci_python/fibonacci.py
+++ week2_algorithic_warm-up/fibonacci/fibonacci_python/fibonacci.py
@@ -20,6 +20,18 @@ def calc_fib_fast(n):
         F[i] = F[i - 1] + F[i - 2]
     return F[n]
 
+def calc_fib_faster(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+
+    return current
+
 if __name__ == '__main__':
     n = int(input())
-    print(calc_fib_fast(n))
+    print(calc_fib_faster(n))
